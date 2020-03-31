f2c8228dcc0746aa453ba002b3b8485f55083ebf
Kevin <kevmensink@hotmail.com>
2020-03-31 09:12:01 +0200

    Initial setup of the repo
    all the stuff is now up to date

---
+++ Assets/Scripts/Glyph.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glyph : MonoBehaviour
+{
+    public bool activated;
+    public string symbol;
+
+    Renderer rend;
+
+    public Material baseMat;
+    public Material selectedMat;
+
+    public Address address;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        symbol = this.name;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Activate()
+    {
+        /*!!!DHD USE ONLY!!!*/
+        if (address.addressList.Count <= 6) /*TODO: if the address has 7 glyphs, disable all glyphs, and only allow the big red button (maybe with a highlight?)*/
+        {
+            if (!activated)
+            {
+                rend.material = selectedMat;
+
+                address.addressList.Add(symbol);
+
+                #region per DHD button press, play a sound related to that button press
+                switch (address.addressList.Count)
+                {
+                    case 1: /*play sound 1*/
+                        FindObjectOfType<AudioManager>().Play($"dhd_usual_{address.addressList.Count}");
+                        break;
+                    case 2: /*play sound 2*/
+                        FindObjectOfType<AudioManager>().Play($"dhd_usual_{address.addressList.Count}");
+                        break;
+                    case 3: /*play sound 3*/
+                        FindObjectOfType<AudioManager>().Play($"dhd_usual_{address.addressList.Count}");
+                        break;
+                    case 4: /*play sound 4*/
+                        FindObjectOfType<AudioManager>().Play($"dhd_usual_{address.addressList.Count}");
+                        break;
+                    case 5: /*play sound 5*/
+                        FindObjectOfType<AudioManager>().Play($"dhd_usual_{address.addressList.Count}");
+                        break;
+                    case 6: /*play sound 6*/
+                        FindObjectOfType<AudioManager>().Play($"dhd_usual_{address.addressList.Count}");
+                        break;
+                    case 7: /*play sound 7*/
+                        FindObjectOfType<AudioManager>().Play($"dhd_usual_{address.addressList.Count}");
+                        break;
+                }
+                #endregion
+
+            }
+            else
+            {
+                rend.material = baseMat;
+                address.addressList.Remove(symbol);
+            }
+        }
+        else
+        {
+            /*do i need this?*/
+        }
+
+        if (!activated && symbol == "BigRedButton")
+        {
+            /*onces the BigRedButton has been pressed, activate the gate aka Create the wormhole or clear the addressList*/
+            if (address.addressList.Count < 6)
+            {
+                address.addressList.Clear();
+
+                /*TODO: foreach glyph that is activated, reset the mat to baseMat*/
+            }
+        }
+        activated = !activated;
+
+        //
+    }
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitiviy = 100f;
+    public Transform playerBody;
+    private float xRotation = 0f;
+
+    public float range = 100f;
+    public Camera fpsCam;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        //Brackeys code <3
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitiviy * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitiviy * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        // my own hacking + brackeys
+        if (Input.GetButtonDown("Fire1"))
+            Shoot();
+    }
+
+    void Shoot()
+    {
+        /*RaycastHit hit;*/
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out RaycastHit hit, range))
+        {
+            Glyph glyph = hit.transform.GetComponent<Glyph>();
+
+            if (glyph != null)
+                glyph.Activate();
+
+
+            /* after the "BigRedButton" is pressed, the address needs to be emptied, and the gate needs to be enabled.*/
+
+
+            #region
+            //for (int i = 0; i < constellationsList.Count; i++)
+            //{
+            //    if (constellationsList[i].name == constTarget)
+            //    {
+            //        Debug.Log(constellationsList[i].name + " " + "The Const Target is: " + constTarget);
+            //    }
+            //}
+            #endregion
+
+
+        }
+    }
+
+}
